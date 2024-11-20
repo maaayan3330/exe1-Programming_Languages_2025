2ef17d2c0e56a26a791e8aea8955fcdee780cc94
AlonLivne <alonlivne95@gmail.com>
2024-11-20 17:11:33 +0200

    aa2

--- Insert_at.ml
+++ Insert_at.ml
@@ -2,6 +2,8 @@
 (*insert function - get index, char ans list : put the char in the index of the list*)
 
 let insert_at n indexn list =
+if indexn == 0 then n :: list
+else 
     let rec aux i = function
         | [] -> []
         | h :: t -> if i = indexn then h::n::t else h :: aux(i + 1) t
