c91900450b7d98b5700b398c75b4484f67a42503
maayan3330 <maayan3330@gmail.com>
2024-11-20 17:19:12 +0200

    commit

--- insert_at.ml
+++ insert_at.ml
@@ -2,8 +2,10 @@
 (*insert function - get index, char ans list : put the char in the index of the list*)
 
 let insert_at n indexn list =
+if indexn == 0 then n :: list
+else 
     let rec aux i = function
         | [] -> []
         | h :: t -> if i = indexn then h::n::t else h :: aux(i + 1) t
     in
-    aux 1 list;; 
+    aux 1 list;;
