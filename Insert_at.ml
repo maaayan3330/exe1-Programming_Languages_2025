47c7cba3c333ae1ef48fb2c9855ee4f8fb1f150f
maayan3330 <maayan3330@gmail.com>
2024-11-20 15:33:48 +0200

    commit

--- Insert_at.ml
+++ Insert_at.ml
@@ -1,3 +1,8 @@
 (*PART - 1: Ocaml*)
 (*insert function - get index, char ans list : put the char in the index of the list*)
 
+let insert_at n indexn list =
+    let rec aux i = function
+        | [] -> []
+        | h :: t -> if i = indexn than h = n else h :: aux(i + 1) t
+        | 
