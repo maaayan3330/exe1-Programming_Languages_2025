ebb96c77c54f86c1f072854962d9dc2d4e548120
maayan3330 <maayan3330@gmail.com>
2024-11-26 20:39:49 +0200

    commit

--- comparator_tree.ml
+++ comparator_tree.ml
@@ -6,4 +6,4 @@ type 'a binary_tree =
 let rec comparator_tree t arg comp_func =
     match t with
     | Empty -> Node (arg,Empty,Empty)
-    | Node(key,l,r) -> if comp_func arg key = false then Node(key, l , comparator_tree r arg comp_func) else Node(key, comparator_tree l arg comp_func, r)
+    | Node(key,l,r) -> if comp_func arg key = true then Node(key, l , comparator_tree r arg comp_func) else Node(key, comparator_tree l arg comp_func, r)
